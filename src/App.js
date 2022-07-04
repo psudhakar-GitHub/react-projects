9e313eb9b75beeb60777f71b8897262b6878e623
Sudhakar <psudhakar77@gmail.com>
2022-07-04 10:41:14 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./App.css";
+import { Button, Container } from "react-bootstrap";
+
+export default function App() {
+  return (
+    <Container className="mt-5">
+      <Button variant="success">Hello World!!</Button>
+    </Container>
+  );
+}
